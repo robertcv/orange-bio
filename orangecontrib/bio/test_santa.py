11ff437c0449edc976e3cf76c9fcc2379332d411
Robert Cvitkovič <robert.cvitkovic@gmail.com>
2017-08-14 22:45:11 +0200

    changed node name from id to official symbol

--- orangecontrib/bio/test_santa.py
+++ orangecontrib/bio/test_santa.py
@@ -7,8 +7,9 @@ import matplotlib.pyplot as plt
 g = nx.Graph()
 g.nodes_iter()
 
-nodes = [i for i in range(1,13)]
-edges = [(1,2), (1,3), (1,4), (2,3), (2,4), (3,4), (1,5), (5,6), (2,7), (7,8), (4,9), (9,10), (3,11), (11,12)]
+nodes = [str(i) for i in range(1,13)]
+edges = [('1','2'), ('1','3'), ('1','4'), ('2','3'), ('2','4'), ('3','4'), ('1','5'), ('5','6'), ('2','7'),
+         ('7','8'), ('4','9'), ('9','10'), ('3','11'), ('11','12')]
 
 for n in nodes:
     g.add_node(n)
@@ -18,7 +19,7 @@ for n1, n2 in edges:
 plt.figure(1)
 nx.draw_networkx(g)
 
-node_w = {1:1, 2:1}
+node_w = {'1':1, '2':1}
 test_santa1 = SANTA(g, node_w)
 k_net, auc_k_net = test_santa1.k_net()
 print('Knet:')
@@ -34,7 +35,7 @@ for n in k_node:
     print(n)
 
 print()
-node_w = {5:1, 6:1}
+node_w = {'5':1, '6':1}
 test_santa2 = SANTA(g, node_w)
 k_net, auc_k_net = test_santa2.k_net()
 print('Knet:')
